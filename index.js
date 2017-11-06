2fb329394ce72edfe68ea2d6fd71361298479be9
Victoria <vitalina27@gmail.com>
2017-11-06 19:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var otherDeli;
 function takeANumber(katzDeli,name){
   katzDeli.push(name);
   console.log( "Welcome, "+name+". You are number "+katzDeli.length+" in line.")
-  return  "Welcome, "+name+". You are number "+katzDeli.length+1+" in line." ;
+  return  "Welcome, "+name+". You are number "+katzDeli.length+" in line." ;
 
 
 }
