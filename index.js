f6c624e94e173e363e6ada8b822db1d7a5689844
Victoria <vitalina27@gmail.com>
2017-11-06 18:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function takeANumber(){}
+var katzDeli=[];
+var otherDeli;
+function takeANumber(name){
+  katzDeli.push(name);
+  return katzDeli.length+1;
+  
+  
+}
 
 function katzDeliLine(){}
 
