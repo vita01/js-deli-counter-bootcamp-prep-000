db29aeed37a2746209014155f222b377b35f3b93
Victoria <vitalina27@gmail.com>
2017-11-06 19:20:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(line){
     return "There is nobody waiting to be served!"
   }
   else{
-    return "Currently serving "+line[0]+".";
+    return "Currently serving "+line.shift()+".";
   }
 
 
